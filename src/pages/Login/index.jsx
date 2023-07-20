41ccd08a844e608b9c6ba638a4e27ceac230ad06
donghyukkil <asterism90@gmail.com>
2023-07-20 23:56:42 +0900

    feat: 로그인 기능 구현

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,43 @@
 import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const signInwithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      if (user) {
+        const tkn = await user.getIdToken();
+        sessionStorage.setItem("accessToken", tkn);
+
+        const fetchData = async (tkn) => {
+          try {
+            const response = await fetch("http://localhost:3000/login", {
+              method: "POST",
+              headers: {
+                Authorization: tkn,
+              },
+            });
+
+            if (response.ok) {
+            } else {
+              throw new Error("요청이 실패했습니다");
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        };
+
+        await fetchData(tkn);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col space-y-40">
@@ -114,6 +151,7 @@ const Login = () => {
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={signInwithGoogle}
                 >
                   sign in with Google
                 </button>
