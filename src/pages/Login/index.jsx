9a376f65a7a9ef7ae0462a9f6ec724ec6eda0ca5
donghyukkil <asterism90@gmail.com>
2023-07-20 17:50:28 +0900

    feat: DropDownMenu를 NavBar에 추가

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -66,6 +66,33 @@ const Login = () => {
                     />
                   </button>
                 </div>
+
+                <div
+                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu-button"
+                  tabIndex="-1"
+                >
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="user-menu-item-0"
+                  >
+                    내 그림 보기
+                  </a>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="user-menu-item-1"
+                  >
+                    로그아웃
+                  </a>
+                </div>
               </div>
             </div>
           </div>
