dd01754f08309cb70324fbf7e5804ef35e2e8b15
donghyukkil <asterism90@gmail.com>
2023-07-21 17:14:59 +0900

    chore: 불 필요한 주석을 firebase-config.js에서 삭제

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,9 +7,6 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// export { app, auth };
