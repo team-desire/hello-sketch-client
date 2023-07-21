8faae83e4d03a4b1e93a317ad8b8a1a9fb062355
donghyukkil <asterism90@gmail.com>
2023-07-22 00:16:32 +0900

    refactor: NavBar에서 프로필 이미지 변경

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -40,8 +40,8 @@ const NavBar = () => {
                 <span className="sr-only">Open user menu</span>
                 <img
                   className="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
+                  src="https://lh3.googleusercontent.com/a/AAcHTtc_iQzeM6UY-xwrPg_jMPl2SxW-7fNaVyoSuEmia-qh=s96-c"
+                  alt="userImg"
                 />
               </button>
             </div>
