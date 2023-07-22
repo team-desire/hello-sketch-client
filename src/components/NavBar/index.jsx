67eafcd85751ddb2d06fd2a9a68ceadae3250c8c
donghyukkil <asterism90@gmail.com>
2023-07-22 15:35:26 +0900

    feat: DropDownMenu를 NavBar에 추가

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { signOut, getAuth } from "firebase/auth";
@@ -5,6 +6,7 @@ import { signOut, getAuth } from "firebase/auth";
 import Button from "../Button";
 
 const NavBar = () => {
+  const [toShowDropDownMenu, setToShowDropDownMenu] = useState(false);
   const navigate = useNavigate();
   const navigateToHome = () => {
     navigate("/");
@@ -41,7 +43,14 @@ const NavBar = () => {
           <div className="relative ml-3">
             <div>
               {!!sessionStorage.accessToken ? (
-                <Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <Button
+                  className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  onClick={() =>
+                    setToShowDropDownMenu(
+                      (prevToShowDropDownMenu) => !prevToShowDropDownMenu,
+                    )
+                  }
+                >
                   <img
                     className="h-8 w-8 rounded-full"
                     src={userPhotoURL}
@@ -51,24 +60,26 @@ const NavBar = () => {
               ) : null}
             </div>
 
-            <div
-              className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="user-menu-button"
-              tabIndex="-1"
-            >
-              <Button style={"block px-4 py-2 text-sm text-gray-700"}>
-                내 그림 보기
-              </Button>
-
-              <Button
-                style={"block px-4 py-2 text-sm text-gray-700"}
-                onClick={logoutUser}
+            {toShowDropDownMenu && (
+              <div
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="user-menu-button"
+                tabIndex="-1"
               >
-                로그아웃
-              </Button>
-            </div>
+                <Button style={"block px-4 py-2 text-sm text-gray-700"}>
+                  내 그림 보기
+                </Button>
+
+                <Button
+                  style={"block px-4 py-2 text-sm text-gray-700"}
+                  onClick={logoutUser}
+                >
+                  로그아웃
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
