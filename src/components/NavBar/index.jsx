8209ef5b5aabb861d672ffc5faa30f18f923daf4
donghyukkil <asterism90@gmail.com>
2023-07-22 19:51:29 +0900

    refactor: a태그, 불필요한 div태그를 NavBar에서 삭제함

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -31,33 +31,33 @@ const NavBar = () => {
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2">
         <div className="relative flex h-16 items-center justify-between">
-          <a
-            href="#"
-            className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-            aria-current="page"
+          <Button
             onClick={navigateToHome}
+            style={
+              "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+            }
           >
             홈으로 가기
-          </a>
-          <div className="relative ml-3">
-            <div>
-              {!!sessionStorage.accessToken ? (
-                <Button
-                  className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  onClick={() =>
-                    setToShowDropDownMenu(
-                      (prevToShowDropDownMenu) => !prevToShowDropDownMenu,
-                    )
-                  }
-                >
-                  <img
-                    className="h-8 w-8 rounded-full"
-                    src={userPhotoURL}
-                    alt="userImage"
-                  />
-                </Button>
-              ) : null}
-            </div>
+          </Button>
+
+          <div>
+            {!!sessionStorage.accessToken ? (
+              <Button
+                className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                onClick={() =>
+                  setToShowDropDownMenu(
+                    (prevToShowDropDownMenu) => !prevToShowDropDownMenu,
+                  )
+                }
+              >
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src={userPhotoURL}
+                  alt="userImage"
+                />
+              </Button>
+            ) : null}
+
             {toShowDropDownMenu && (
               <div
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
