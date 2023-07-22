7a4a72bd12993885f1c18dceda45338b7488c4cc
donghyukkil <asterism90@gmail.com>
2023-07-22 15:59:47 +0900

    style: 개행을 NavBar에서 수정

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -39,7 +39,6 @@ const NavBar = () => {
           >
             홈으로 가기
           </a>
-
           <div className="relative ml-3">
             <div>
               {!!sessionStorage.accessToken ? (
@@ -59,7 +58,6 @@ const NavBar = () => {
                 </Button>
               ) : null}
             </div>
-
             {toShowDropDownMenu && (
               <div
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
@@ -71,7 +69,6 @@ const NavBar = () => {
                 <Button style={"block px-4 py-2 text-sm text-gray-700"}>
                   내 그림 보기
                 </Button>
-
                 <Button
                   style={"block px-4 py-2 text-sm text-gray-700"}
                   onClick={logoutUser}
