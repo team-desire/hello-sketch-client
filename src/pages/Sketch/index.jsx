89a118cf36de16c8fc19197c5c2e0099cbfd32ae
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-25 14:29:52 +0900

    feat: Sketch 페이지 레이아웃 구성

--- src/pages/Sketch/index.jsx
+++ src/pages/Sketch/index.jsx
@@ -4,8 +4,26 @@ import SubNavBar from "../../components/SubNavBar";
 const Sketch = () => {
   return (
     <>
-      <NavBar />
-      <SubNavBar />
+      <div className="flex flex-col min-h-screen">
+        <NavBar />
+        <SubNavBar />
+        <div className="flex justify-center my-6">
+          <input
+            className="placeholder:text-slate-400 w-96 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
+            placeholder="Title"
+            type="text"
+            name="title"
+          />
+        </div>
+        <div className="flex flex-grow">
+          <div className="w-1/3 bg-gray-200 p-4">
+            <p>This is the left column.</p>
+          </div>
+          <div className="w-2/3 bg-gray-300 p-4">
+            <p>This is the right column.</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
