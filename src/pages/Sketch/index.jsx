32797543503760d510af9ff6b70dccde0e4e8165
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-26 15:23:58 +0900

    feat: Canvas UI 생성

--- src/pages/Sketch/index.jsx
+++ src/pages/Sketch/index.jsx
@@ -9,14 +9,6 @@ const Sketch = () => {
       <div className="flex flex-col min-h-screen">
         <NavBar />
         <SubNavBar />
-        <div className="flex justify-center my-6">
-          <input
-            className="placeholder:text-slate-400 w-96 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
-            placeholder="Title"
-            type="text"
-            name="title"
-          />
-        </div>
         <div className="flex flex-grow">
           <UnitSelector style={"w-1/2 bg-gray-200 p-4"} />
           <Canvas style={"w-1/2 bg-gray-300 p-4"} />
