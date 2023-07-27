c3ebe715ec70f780d469c04c378751bea3e53620
mangodm-web <mangodm.web3@gmail.com>
2023-07-27 19:01:22 +0900

    feat: s3에서 가져온 svg 데이터를 캐러셀에 props로 전달하는 로직 추가

--- src/components/UnitSelector/index.jsx
+++ src/components/UnitSelector/index.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import ColorPicker from "../ColorPicker";
 import Carousel from "../Carousel";
 
 import { TOTAL_PAGES } from "./UnitSelector.mock";
+import { fetchData } from "../../utils/fetchS3Objects";
 
 const UnitSelector = ({ units, unitType, title }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [color, setColor] = useState("#000000");
+  const [svgData, setSvgData] = useState(null);
 
   const handleNextButtonClick = () => {
     if (currentPage < TOTAL_PAGES) {
@@ -25,17 +27,33 @@ const UnitSelector = ({ units, unitType, title }) => {
     setColor(event.target.value);
   };
 
+  useEffect(() => {
+    const fetchDataAndSetData = async () => {
+      try {
+        const svgDataArray = await fetchData(units);
+        setSvgData(svgDataArray);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchDataAndSetData();
+  }, [units]);
+
   return (
     <div className="h-1/3 bg-gray-300 border-t border-gray-200 mx-auto px-10">
       <h1 className="text-xl">{title}</h1>
       <ColorPicker color={color} onColorChange={handleColorChange} />
-      <Carousel
-        items={units}
-        currentPage={currentPage}
-        totalPages={TOTAL_PAGES}
-        onPrevButtonClick={handlePrevButtonClick}
-        onNextButtonClick={handleNextButtonClick}
-      />
+      {svgData && (
+        <Carousel
+          items={svgData}
+          currentPage={currentPage}
+          totalPages={TOTAL_PAGES}
+          onPrevButtonClick={handlePrevButtonClick}
+          onNextButtonClick={handleNextButtonClick}
+          color={color}
+        />
+      )}
     </div>
   );
 };
