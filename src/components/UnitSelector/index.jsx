029844f225337fb5d98d0ac22a51c0a217b6a8d2
mangodm-web <mangodm.web3@gmail.com>
2023-07-27 16:19:35 +0900

    feat: UnitSelector 컴포넌트 추가

--- src/components/UnitSelector/index.jsx
+++ src/components/UnitSelector/index.jsx
@@ -1,5 +1,43 @@
-const UnitSelector = ({ style }) => {
-  return <div className={style}></div>;
+import { useState } from "react";
+
+import ColorPicker from "../ColorPicker";
+import Carousel from "../Carousel";
+
+import { TOTAL_PAGES } from "./UnitSelector.mock";
+
+const UnitSelector = ({ units, unitType, title }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [color, setColor] = useState("#000000");
+
+  const handleNextButtonClick = () => {
+    if (currentPage < TOTAL_PAGES) {
+      setCurrentPage((currentPage) => currentPage + 1);
+    }
+  };
+
+  const handlePrevButtonClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage((currentPage) => currentPage - 1);
+    }
+  };
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  return (
+    <div className="h-1/3 bg-gray-300 border-t border-gray-200 mx-auto px-10">
+      <h1 className="text-xl">{title}</h1>
+      <ColorPicker color={color} onColorChange={handleColorChange} />
+      <Carousel
+        items={units}
+        currentPage={currentPage}
+        totalPages={TOTAL_PAGES}
+        onPrevButtonClick={handlePrevButtonClick}
+        onNextButtonClick={handleNextButtonClick}
+      />
+    </div>
+  );
 };
 
 export default UnitSelector;
