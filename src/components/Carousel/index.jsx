6bb33fcf6cd2ef4d800a0754d750013c39ef380f
mangodm-web <mangodm.web3@gmail.com>
2023-07-27 16:19:52 +0900

    feat: Carousel 컴포넌트 추가

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,65 +1,50 @@
-import { useEffect, useState } from "react";
 import { AiOutlineCaretRight, AiOutlineCaretLeft } from "react-icons/ai";
 
 import Button from "../Button";
 
-const Carousel = () => {
-  const [images, setImages] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(null);
-
-  useEffect(() => {
-    const fetchSketches = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:3000/sketches?per_page=3&page=${currentPage}`,
-        );
-
-        const sketches = await response.json();
-        setImages(sketches.sketchesUrl.list);
-        setTotalPages(sketches.sketchesUrl.totalPages);
-      } catch (error) {
-        console.error("Error fetching sketches:", error.message);
-      }
-    };
-
-    fetchSketches();
-  }, [currentPage]);
-
-  const handlePrev = () => {
-    setCurrentPage((currentPage) => currentPage - 1);
-  };
-
-  const handleNext = () => {
-    setCurrentPage((currentPage) => currentPage + 1);
-  };
-
+const Carousel = ({
+  items,
+  currentPage,
+  totalPages,
+  itemsPerPage = 3,
+  onPrevButtonClick,
+  onNextButtonClick,
+}) => {
   return (
-    <div className="flex justify-evenly">
-      <Button
-        onClick={handlePrev}
-        disabled={currentPage === 1}
-        style={`${currentPage === 1 && "disabled:opacity-25"}`}
-      >
-        {<AiOutlineCaretLeft size={40} />}
-      </Button>
-      <div className="flex my-16">
-        {images.map((item, index) => (
-          <img
-            className="w-20 mx-6"
-            key={item.imageUrl}
-            src={item.imageUrl}
-            alt={`Image ${index + 1}`}
-          />
-        ))}
+    <div className="container mx-auto p-4">
+      <div className="flex items-center justify-center w-full">
+        <Button
+          onClick={onPrevButtonClick}
+          disabled={currentPage === 1}
+          style={`ml-8 ${"disabled:opacity-25"}`}
+        >
+          {<AiOutlineCaretLeft size={40} />}
+        </Button>
+        <div className="flex space-x-5">
+          {Array.from({ length: itemsPerPage }).map((item, index) => (
+            <div key={index} className="w-1/3">
+              {items[index] ? (
+                <div className="bg-gray-200 rounded-lg w-52 h-52">
+                  <img
+                    src={items[index]}
+                    alt={`Item ${index + 1}`}
+                    className="w-full h-full rounded-lg"
+                  />
+                </div>
+              ) : (
+                <div className="w-52 h-52 bg-gray-200 rounded-lg"></div>
+              )}
+            </div>
+          ))}
+        </div>
+        <Button
+          onClick={onNextButtonClick}
+          disabled={currentPage === totalPages}
+          style={`mr-8 ${"disabled:opacity-25"}`}
+        >
+          {<AiOutlineCaretRight size={40} />}
+        </Button>
       </div>
-      <Button
-        onClick={handleNext}
-        disabled={currentPage === totalPages}
-        style={` ${currentPage === totalPages && "disabled:opacity-25"}`}
-      >
-        {<AiOutlineCaretRight size={40} />}
-      </Button>
     </div>
   );
 };
