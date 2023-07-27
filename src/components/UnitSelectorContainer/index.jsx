c0c245b9f032981124117726e9714f02bb5c53b4
mangodm-web <mangodm.web3@gmail.com>
2023-07-27 16:19:11 +0900

    feat: UnitSelectorContainer 컴포넌트 추가

--- src/components/UnitSelectorContainer/index.jsx
+++ src/components/UnitSelectorContainer/index.jsx
@@ -1,11 +1,18 @@
 import UnitSelector from "../UnitSelector";
 
+// NOTE: 현재는 mock data 활용중!
+import {
+  BODY_UNITS,
+  FACE_UNITS,
+  HEAD_UNITS,
+} from "../UnitSelector/UnitSelector.mock";
+
 const UnitSelectorContainer = ({ style }) => {
   return (
     <div className={style}>
-      <UnitSelector title="Head" unitType="head" />
-      <UnitSelector title="Body" unitType="body" />
-      <UnitSelector title="Bottom" unitType="bottom" />
+      <UnitSelector units={HEAD_UNITS} title="Head" unitType="head" />
+      <UnitSelector units={FACE_UNITS} title="Face" unitType="face" />
+      <UnitSelector units={BODY_UNITS} title="Body" unitType="body" />
     </div>
   );
 };
