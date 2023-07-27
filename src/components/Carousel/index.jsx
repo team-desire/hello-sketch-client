752498d21dde8fff88003e3d325c5fb0a719abee
mangodm-web <mangodm.web3@gmail.com>
2023-07-27 19:00:13 +0900

    feat: Carousel 컴포넌트에 Unit 컴포넌트 추가

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,6 +1,7 @@
 import { AiOutlineCaretRight, AiOutlineCaretLeft } from "react-icons/ai";
 
 import Button from "../Button";
+import Unit from "../Unit";
 
 const Carousel = ({
   items,
@@ -9,6 +10,7 @@ const Carousel = ({
   itemsPerPage = 3,
   onPrevButtonClick,
   onNextButtonClick,
+  color = undefined,
 }) => {
   return (
     <div className="container mx-auto p-4">
@@ -25,10 +27,10 @@ const Carousel = ({
             <div key={index} className="w-1/3">
               {items[index] ? (
                 <div className="bg-gray-200 rounded-lg w-52 h-52">
-                  <img
-                    src={items[index]}
-                    alt={`Item ${index + 1}`}
-                    className="w-full h-full rounded-lg"
+                  <Unit
+                    svgData={items[index]}
+                    selectedColor={color}
+                    style="w-full h-full rounded-lg"
                   />
                 </div>
               ) : (
