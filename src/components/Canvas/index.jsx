034f06cb13f9982f066e3b1a81d61ee7b4bcea18
donghyukkil <asterism90@gmail.com>
2023-07-27 11:35:24 +0900

    feat: bounding box, 이미지 이동 범위 제한 기능을 MovealbleSVG에 추가

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -1,9 +1,12 @@
 import { useRef } from "react";
+
 import MoveableSVG from "../MoveableSVG";
+
 const Canvas = ({ style, sketchTitle, onChange }) => {
   const containerRef = useRef(null);
+
   return (
-    <div className={style} ref={containerRef}>
+    <div className={style}>
       <div className="flex justify-center my-6">
         <input
           className="placeholder:text-slate-400 w-96 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
@@ -14,7 +17,10 @@ const Canvas = ({ style, sketchTitle, onChange }) => {
           onChange={onChange}
         />
       </div>
-      <div className="box-content h-4/5 p-4 border-4 bg-white">
+      <div
+        className="box-content h-4/5 p-4 border-4 bg-white"
+        ref={containerRef}
+      >
         <MoveableSVG containerRef={containerRef} />
       </div>
     </div>
--- src/components/MoveableSVG/index.jsx
+++ src/components/MoveableSVG/index.jsx
@@ -4,31 +4,33 @@ const MoveableSVG = ({ containerRef }) => {
   const [selectedSVGs, setSelectedSVGs] = useState(Array(3).fill(null));
   const [isDragging, setIsDragging] = useState(Array(3).fill(false));
   const [boundingBoxs, setBoundingBoxs] = useState(Array(3).fill(null));
-
+  const [boundingBoxVisible, setBoundingBoxVisible] = useState(
+    Array(3).fill(false),
+  );
   const [offsets, setOffsets] = useState(Array(3).fill({ x: 0, y: 0 }));
   const [positions, setPositions] = useState([
-    { x: 0, y: 0 }, // 첫 번째 SVG의 초기 위치값
-    { x: 20, y: 20 }, // 두 번째 SVG의 초기 위치값
-    { x: 40, y: 40 }, // 세 번째 SVG의 초기 위치값
+    { x: 0, y: 0 },
+    { x: 20, y: 20 },
+    { x: 40, y: 40 },
   ]);
   const [positionRatios, setPositionRatios] = useState(
     Array(3).fill({ x: 0, y: 0 }),
   );
-
   const svgRefs = useRef([null, null, null]);
 
   const handleSVGSelect = (index) => (e) => {
     const file = e.target.files[0];
+
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
         const newSelectedSVGs = [...selectedSVGs];
         newSelectedSVGs[index] = reader.result;
         setSelectedSVGs(newSelectedSVGs);
-
         const parser = new DOMParser();
         const svgDoc = parser.parseFromString(reader.result, "image/svg+xml");
         const layerElement = svgDoc.querySelector("#layer1");
+
         if (layerElement) {
           const bbox = layerElement.getBBox();
           const newBoundingBoxs = [...boundingBoxs];
@@ -51,32 +53,33 @@ const MoveableSVG = ({ containerRef }) => {
       x: containerBounds.width * positionRatios[index].x,
       y: containerBounds.height * positionRatios[index].y,
     };
-
     const newPositions = [...positions];
     newPositions[index] = newPosition;
     setPositions(newPositions);
   };
 
   const handleMouseDown = (index) => (e) => {
-    const newIsDragging = [...isDragging];
-    newIsDragging[index] = true;
-    setIsDragging(newIsDragging);
-
+    setIsDragging((prev) => prev.map((_, i) => i === index));
     const newOffset = {
       x: e.clientX - positions[index].x,
       y: e.clientY - positions[index].y,
     };
-    const newOffsets = [...offsets];
-    newOffsets[index] = newOffset;
-    setOffsets(newOffsets);
+    setOffsets((prev) =>
+      prev.map((offset, i) => (i === index ? newOffset : offset)),
+    );
+    setBoundingBoxVisible((prev) =>
+      prev.map((visible, i) => (i === index ? !visible : visible)),
+    );
   };
 
   const updateBoundingBox = useCallback(() => {
     boundingBoxs.forEach((box, index) => {
       if (box) {
         const svgElement = svgRefs.current[index];
+
         if (svgElement) {
           const layerElement = svgElement.querySelector("#layer1");
+
           if (layerElement) {
             const ctm = layerElement.getScreenCTM();
             const newBox = {
@@ -85,6 +88,7 @@ const MoveableSVG = ({ containerRef }) => {
               width: box.width * ctm.a,
               height: box.height * ctm.d,
             };
+
             const newBoundingBoxs = [...boundingBoxs];
             newBoundingBoxs[index] = newBox;
             setBoundingBoxs(newBoundingBoxs);
@@ -95,25 +99,28 @@ const MoveableSVG = ({ containerRef }) => {
   }, [boundingBoxs]);
 
   const handleMouseMove = (e) => {
-    isDragging.forEach((dragging, index) => {
-      if (dragging) {
-        const newX = e.clientX - offsets[index].x;
-        const newY = e.clientY - offsets[index].y;
+    setIsDragging((prev) =>
+      prev.map((dragging, index) => {
+        if (dragging) {
+          let newX = e.clientX - offsets[index].x;
+          let newY = e.clientY - offsets[index].y;
+          const containerBounds = containerRef.current.getBoundingClientRect();
+          const svgBounds = svgRefs.current[index].getBoundingClientRect();
 
-        const containerBounds = containerRef.current.getBoundingClientRect();
-        const svgBounds = svgRefs.current[index].getBoundingClientRect();
+          if (newX < 0) newX = 0;
 
-        if (
-          newX > 0 &&
-          newY > 0 &&
-          newX + svgBounds.width < containerBounds.width &&
-          newY + svgBounds.height < containerBounds.height
-        ) {
+          if (newY < 0) newY = 0;
+
+          if (newX + svgBounds.width > containerBounds.width)
+            newX = containerBounds.width - svgBounds.width;
+
+          if (newY + svgBounds.height > containerBounds.height)
+            newY = containerBounds.height - svgBounds.height;
+
           const newPosition = { x: newX, y: newY };
           const newPositions = [...positions];
           newPositions[index] = newPosition;
           setPositions(newPositions);
-
           const newPositionRatio = {
             x: newX / containerBounds.width,
             y: newY / containerBounds.height,
@@ -121,40 +128,41 @@ const MoveableSVG = ({ containerRef }) => {
           const newPositionRatios = [...positionRatios];
           newPositionRatios[index] = newPositionRatio;
           setPositionRatios(newPositionRatios);
-
           svgRefs.current[
             index
           ].style.transform = `translate(${newX}px, ${newY}px)`;
         }
-      }
-    });
-    updateBoundingBox();
+
+        return dragging;
+      }),
+    );
+
+    if (isDragging.includes(true)) {
+      updateBoundingBox();
+    }
   };
 
   const handleMouseUp = () => {
     setIsDragging(Array(3).fill(false));
   };
 
-  useEffect(() => {
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-
+  const handleResize = useCallback(() => {
     selectedSVGs.forEach((svg, index) => {
       adjustPosition(index);
     });
-    window.addEventListener("resize", () => {
-      selectedSVGs.forEach((svg, index) => {
-        adjustPosition(index);
-      });
-    });
+  }, [selectedSVGs, adjustPosition]);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+    window.addEventListener("resize", handleResize);
 
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseup", handleMouseUp);
-      window.removeEventListener("resize", adjustPosition);
-      updateBoundingBox();
+      window.removeEventListener("resize", handleResize);
     };
-  }, [isDragging, offsets, positionRatios, selectedSVGs, updateBoundingBox]);
+  }, [handleMouseMove, handleMouseUp, handleResize]);
 
   return (
     <div className="moveable-svg-container w-max h-max">
@@ -163,7 +171,6 @@ const MoveableSVG = ({ containerRef }) => {
         .map((_, index) => (
           <div key={index}>
             <input type="file" onChange={handleSVGSelect(index)} />
-
             {selectedSVGs[index] && (
               <div
                 id={`moveable-svg-${index}`}
@@ -171,6 +178,8 @@ const MoveableSVG = ({ containerRef }) => {
                 className="moveable-svg"
                 style={{
                   transform: `translate(${positions[index].x}px, ${positions[index].y}px)`,
+                  border: boundingBoxVisible[index] ? "2px solid red" : "none",
+                  boxSizing: "border-box",
                 }}
                 onMouseDown={handleMouseDown(index)}
                 dangerouslySetInnerHTML={{ __html: selectedSVGs[index] }}
