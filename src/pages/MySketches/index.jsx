7cd905677a9a7b18bff7576b586e6d3f62f5f7ef
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-29 16:48:17 +0900

    feat: MySketches 페이지 레이아웃 구성

--- src/pages/MySketches/index.jsx
+++ src/pages/MySketches/index.jsx
@@ -1,7 +1,21 @@
 import NavBar from "../../components/NavBar";
 
 const MySketches = () => {
-  return <NavBar />;
+  return (
+    <div className="flex flex-col h-screen">
+      <NavBar />
+      <main className="flex-grow grid grid-cols-3 grid-rows-2 gap-4 p-4">
+        {Array(6)
+          .fill(null)
+          .map((_, index) => (
+            <div key={index} className="border p-4"></div>
+          ))}
+      </main>
+      <footer className="bg-zinc-100 h-20 flex justify-center items-center">
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
 };
 
 export default MySketches;
