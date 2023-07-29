b44798bd5bb06ac73bea8d7fbd1ecc76ec24462d
mangodm-web <mangodm.web3@gmail.com>
2023-07-29 11:46:17 +0900

    feat: 유닛 이미지 s3에서 가져오도록 수정

--- src/utils/getSvgDataArray.js
+++ src/utils/getSvgDataArray.js
@@ -1,21 +1,15 @@
-import { BODY } from "../mocks/body";
-import { FACE } from "../mocks/face";
-import { HEAD } from "../mocks/head";
-
 const getSvgDataArray = async (urls) => {
   try {
-    // const fetchPromises = urls.map(async (url) => {
-    //   const response = await fetch(url, { mode: "no-cors" });
-    //   const data = await response.text();
-    //   return data;
-    // });
-    // const svgDataArray = await Promise.all(fetchPromises);
-    // return svgDataArray;
+    const svgDataArray = await Promise.all(
+      urls.map(async (url) => {
+        const response = await fetch(url);
+        return response.text();
+      }),
+    );
 
-    return [HEAD, FACE, BODY];
+    return svgDataArray;
   } catch (error) {
-    console.error(error);
-    console.error("Error fetching s3 objects");
+    console.error("Error fetching data");
   }
 };
 
