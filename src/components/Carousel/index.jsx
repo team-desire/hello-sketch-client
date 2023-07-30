217a6d9508ef9180b098307b93e518c1f889f088
mangodm-web <mangodm.web3@gmail.com>
2023-07-30 20:16:51 +0900

    feat: Carousel에서 png, svg 렌더링 가능하도록 변경

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,7 +1,7 @@
 import { AiOutlineCaretRight, AiOutlineCaretLeft } from "react-icons/ai";
 
 import Button from "../Button";
-import Unit from "../Unit";
+import ChildCanvas from "../ChildCanvas";
 
 const Carousel = ({
   items,
@@ -10,7 +10,10 @@ const Carousel = ({
   itemsPerPage = 3,
   onPrevButtonClick,
   onNextButtonClick,
-  color = undefined,
+  fillColor,
+  elements,
+  onElementChange,
+  unitType,
 }) => {
   return (
     <div className="container mx-auto p-4">
@@ -20,31 +23,47 @@ const Carousel = ({
           disabled={currentPage === 1}
           style={`ml-8 ${"disabled:opacity-25"}`}
         >
-          {<AiOutlineCaretLeft size={40} />}
+          <AiOutlineCaretLeft size={40} />
         </Button>
         <div className="flex space-x-5">
-          {Array.from({ length: itemsPerPage }).map((item, index) => (
-            <div key={index} className="w-1/3">
-              {items[index] ? (
-                <div className="bg-gray-200 rounded-lg w-52 h-52">
-                  <Unit
-                    svgData={items[index]}
-                    selectedColor={color}
-                    style="w-full h-full rounded-lg"
-                  />
-                </div>
-              ) : (
-                <div className="w-52 h-52 bg-gray-200 rounded-lg"></div>
-              )}
-            </div>
-          ))}
+          {Array.from({ length: itemsPerPage }).map((_, index) => {
+            const item = items[index];
+
+            return (
+              <div key={index} className="w-1/3">
+                {item && typeof item === "string" ? (
+                  <div className="bg-gray-200 rounded-lg w-52 h-52">
+                    {item.includes("<svg") ? (
+                      <ChildCanvas
+                        svgData={item}
+                        width={200}
+                        height={200}
+                        fillColor={fillColor}
+                        elements={elements}
+                        onElementChange={onElementChange}
+                        unitType={unitType}
+                      />
+                    ) : (
+                      <img
+                        src={item}
+                        alt={`Item ${index + 1}`}
+                        className="w-full h-full rounded-lg"
+                      />
+                    )}
+                  </div>
+                ) : (
+                  <div className="w-52 h-52 bg-gray-200 rounded-lg"></div>
+                )}
+              </div>
+            );
+          })}
         </div>
         <Button
           onClick={onNextButtonClick}
           disabled={currentPage === totalPages}
           style={`mr-8 ${"disabled:opacity-25"}`}
         >
-          {<AiOutlineCaretRight size={40} />}
+          <AiOutlineCaretRight size={40} />
         </Button>
       </div>
     </div>
