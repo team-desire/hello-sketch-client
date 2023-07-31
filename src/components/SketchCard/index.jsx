3f1784f955e86574fc0e417e7f81e83c557bf775
donghyukkil <asterism90@gmail.com>
2023-07-31 20:37:46 +0900

    feat: 페이지네이션 데이터를 SketchCard에 추가

--- src/components/SketchCard/index.jsx
+++ src/components/SketchCard/index.jsx
@@ -1,13 +1,21 @@
-const SketchCard = () => {
+const SketchCard = ({ sketch }) => {
+  if (!sketch) {
+    return <div className="border p-4 flex flex-col h-full"></div>;
+  }
+
   return (
     <div className="border p-4 flex flex-col h-full">
       <div className="p-4 flex-grow flex flex-col justify-center items-center bg-zinc-100">
-        <img className="mb-4 max-h-full" />
+        <img className="mb-4 max-h-full" src={`${sketch.url}`} />
       </div>
       <div className="flex flex-col flex-shrink">
-        <h2 className="mb-2 text-lg">Title: </h2>
-        <p className="text-sm">Created: </p>
-        <p className="text-sm">Updated: </p>
+        <h2 className="mb-2 text-lg">Title: {sketch.title} </h2>
+        <p className="text-sm">
+          Created: {new Date(sketch.createdAt).toLocaleString()}{" "}
+        </p>
+        <p className="text-sm">
+          Updated: {new Date(sketch.updatedAt).toLocaleString()}
+        </p>
       </div>
     </div>
   );
