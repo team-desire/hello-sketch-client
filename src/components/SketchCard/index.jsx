87594b85247bf8826d8399c0f9b1918d48036cd4
donghyukkil <asterism90@gmail.com>
2023-07-31 21:31:31 +0900

    feat: SketchCard에서 img 최대 너비, 최대 높이를 제한

--- src/components/SketchCard/index.jsx
+++ src/components/SketchCard/index.jsx
@@ -6,7 +6,11 @@ const SketchCard = ({ sketch }) => {
   return (
     <div className="border p-4 flex flex-col h-full">
       <div className="p-4 flex-grow flex flex-col justify-center items-center bg-zinc-100">
-        <img className="mb-4 max-h-full" src={`${sketch.url}`} />
+        <img
+          className="mb-4 max-h-full"
+          src={`${sketch.url}`}
+          style={{ maxWidth: "500px", maxHeight: "300px" }}
+        />
       </div>
       <div className="flex flex-col flex-shrink">
         <h2 className="mb-2 text-lg">Title: {sketch.title} </h2>
