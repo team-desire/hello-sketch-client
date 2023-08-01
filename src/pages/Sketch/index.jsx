3b211cd6e1897313a7b9d51e9ec62e7a4e8ea462
mangodm-web <mangodm.web3@gmail.com>
2023-08-02 03:45:17 +0900

    chore: 사용되지 않는 컴포넌트 임포트 삭제

--- src/pages/Sketch/index.jsx
+++ src/pages/Sketch/index.jsx
@@ -1,23 +1,17 @@
 import { useEffect, useState } from "react";
 
 import NavBar from "../../components/NavBar";
-import SubNavBar from "../../components/SubNavBar";
 import CanvasContainer from "../../components/CanvasContainer";
 import UnitSelectorContainer from "../../components/UnitSelectorContainer";
 
 import getSvgDataArray from "../../utils/getSvgDataArray";
 
 const Sketch = () => {
-  const [sketchTitle, setSketchTitle] = useState("");
   const [elements, setElements] = useState({
     head: { svgData: null, fillColor: "#000000" },
     face: { svgData: null, fillColor: "#000000" },
     body: { svgData: null, fillColor: "#000000" },
   });
-
-  const handleSketchTitle = (event) => {
-    setSketchTitle(event.target.value);
-  };
 
   const handleElementChange = (unitType, newElementData) => {
     setElements((prevElements) => ({
@@ -62,19 +56,13 @@ const Sketch = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-      <SubNavBar />
       <div className="flex flex-grow">
         <UnitSelectorContainer
           style={"w-1/2 bg-gray-200 p-4"}
           elements={elements}
           onElementChange={handleElementChange}
         />
-        <CanvasContainer
-          style={"w-1/2 bg-gray-300 p-4"}
-          sketchTitle={sketchTitle}
-          onSketchTitleChange={handleSketchTitle}
-          elements={elements}
-        />
+        <CanvasContainer style={"w-1/2 bg-gray-300 p-4"} elements={elements} />
       </div>
     </div>
   );
