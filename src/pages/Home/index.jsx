e6b78a01ac87e65028fe104171d74371551cfce5
mangodm-web <mangodm.web3@gmail.com>
2023-08-02 06:21:52 +0900

    feat: 홈 로그인, 스케치 생성 버튼 스타일 변경

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -118,7 +118,7 @@ const Home = () => {
                       navigate("/sketch/new");
                     }}
                     style={
-                      "flex w-full justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      "flex w-full justify-center rounded-md bg-blue-500 hover:bg-blue-700 px-3 py-2 text-md font-semibold leading-6 text-white"
                     }
                   >
                     Create Sketch
@@ -128,7 +128,7 @@ const Home = () => {
                 <Button
                   onClick={signInwithGoogle}
                   style={
-                    "flex w-full justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    "flex w-full justify-center rounded-md bg-blue-500 hover:bg-blue-700 px-3 py-2 text-md font-semibold leading-6 text-white"
                   }
                 >
                   Sign in with Google
