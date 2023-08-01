99e61c94c7d93735eb27fc28573f75b5ac076ebf
donghyukkil <asterism90@gmail.com>
2023-08-01 12:00:00 +0900

    refactor: 삼항연산자를 이용한 조건부렌더링을 SketchCard에 구현

--- src/components/SketchCard/index.jsx
+++ src/components/SketchCard/index.jsx
@@ -1,9 +1,5 @@
 const SketchCard = ({ sketch }) => {
-  if (!sketch) {
-    return <div className="border p-4 flex flex-col h-full"></div>;
-  }
-
-  return (
+  return sketch ? (
     <div className="border p-4 flex flex-col h-full">
       <div className="p-4 flex-grow flex flex-col justify-center items-center bg-zinc-100">
         <img
@@ -15,13 +11,15 @@ const SketchCard = ({ sketch }) => {
       <div className="flex flex-col flex-shrink">
         <h2 className="mb-2 text-lg">Title: {sketch.title} </h2>
         <p className="text-sm">
-          Created: {new Date(sketch.createdAt).toLocaleString()}{" "}
+          Created: {new Date(sketch.createdAt).toLocaleString()}
         </p>
         <p className="text-sm">
           Updated: {new Date(sketch.updatedAt).toLocaleString()}
         </p>
       </div>
     </div>
+  ) : (
+    <div className="border p-4 flex flex-col h-full"></div>
   );
 };
 
