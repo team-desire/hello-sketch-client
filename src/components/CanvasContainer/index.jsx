7cf98081965527b6b0270524c1e16ac9549b79fb
mangodm-web <mangodm.web3@gmail.com>
2023-08-02 03:45:46 +0900

    feat: 스케치 저장 로직 추가

--- src/components/CanvasContainer/index.jsx
+++ src/components/CanvasContainer/index.jsx
@@ -1,14 +1,12 @@
 import { useState, useRef } from "react";
 
+import { BsDownload, BsToggleOn, BsToggleOff } from "react-icons/bs";
+
 import ParentCanvas from "../ParentCanvas";
 import CanvasUnit from "../CanvasUnit";
+import Button from "../Button";
 
-const CanvasContainer = ({
-  style,
-  sketchTitle,
-  onSketchTitleChange,
-  elements,
-}) => {
+const CanvasContainer = ({ style, elements }) => {
   const canvasRefs = {
     headCanvas: useRef(null),
     faceCanvas: useRef(null),
@@ -36,7 +34,14 @@ const CanvasContainer = ({
     x: 250,
   });
 
-  const mergeCanvasAndSave = () => {
+  const [sketchForm, setSketchForm] = useState({
+    title: "",
+    type: "cartoon",
+    image: null,
+    isPublic: true,
+  });
+
+  const mergeCanvas = () => {
     const headCanvas = canvasRefs.headCanvas.current;
     const faceCanvas = canvasRefs.faceCanvas.current;
     const bodyCanvas = canvasRefs.bodyCanvas.current;
@@ -54,6 +59,12 @@ const CanvasContainer = ({
     mergedContext.drawImage(faceCanvas, faceUnit.x, faceUnit.y);
 
     const mergedDataURL = mergedCanvas.toDataURL("image/png");
+
+    return mergedDataURL;
+  };
+
+  const handleImageDownload = () => {
+    const mergedDataURL = mergeCanvas();
     downloadImage(mergedDataURL);
   };
 
@@ -64,18 +75,93 @@ const CanvasContainer = ({
     link.click();
   };
 
+  const handleSketchSave = async () => {
+    const mergedDataURL = mergeCanvas();
+
+    const requestBody = {
+      title: sketchForm.title,
+      image: mergedDataURL,
+      isPublic: sketchForm.isPublic,
+      type: sketchForm.type,
+    };
+
+    const userId = sessionStorage.getItem("userEmail");
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/users/${userId}/sketches`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log("Server response:", data.sketch.imageUrl);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
   return (
     <div className={style}>
       <div className="flex justify-center my-6">
         <input
-          className="placeholder:text-slate-400 w-96 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
+          className="placeholder:text-slate-400 w-3/5 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 sm:text-sm"
           placeholder="Title"
           type="text"
           name="title"
-          value={sketchTitle}
-          onChange={onSketchTitleChange}
+          value={sketchForm.title}
+          onChange={(event) =>
+            setSketchForm({ ...sketchForm, title: event.target.value })
+          }
         />
-        <button onClick={mergeCanvasAndSave}>Download Sketch </button>
+      </div>
+      <div className="flex justify-around my-6">
+        <div className="flex items-center">
+          {sketchForm.isPublic ? "Public" : "Private"}
+          <Button
+            onClick={() => {
+              setSketchForm({ ...sketchForm, isPublic: !sketchForm.isPublic });
+            }}
+          >
+            {sketchForm.isPublic ? (
+              <BsToggleOn size={25} />
+            ) : (
+              <BsToggleOff size={25} />
+            )}
+          </Button>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <Button
+          onClick={handleImageDownload}
+          style={
+            "flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white rounded-md px-3 py-2 text-md"
+          }
+        >
+          <BsDownload className="mr-2" />
+          Download
+        </Button>
+        <Button
+          onClick={handleSketchSave}
+          style={`
+                  flex items-center 
+                  justify-center bg-blue-500 
+                  hover:bg-blue-700 
+                  text-white 
+                  rounded-md 
+                  px-3 py-2 
+                  text-md`}
+        >
+          Save
+        </Button>
       </div>
       <ParentCanvas width={700} height={700}>
         <CanvasUnit
