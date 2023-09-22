05e1e7d44f0bf926647716b33c27b27be9ca10bf
donghyukkil <asterism90@gmail.com>
2023-09-22 14:29:03 +0900

    fix: totalPages 0일 때 렌더링 방지

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -27,37 +27,38 @@ const Carousel = ({
           <AiOutlineCaretLeft size={40} />
         </Button>
         <div className="flex space-x-5">
-          {Array.from({ length: itemsPerPage }).map((_, index) => {
-            const item = items[index];
+          {totalPages !== 0 &&
+            Array.from({ length: itemsPerPage }).map((_, index) => {
+              const item = items[index];
 
-            return (
-              <div key={index} className="w-1/3">
-                {item && typeof item === "string" ? (
-                  <div className="bg-gray-200 rounded-lg w-52 h-52">
-                    {item.includes("<svg") ? (
-                      <ChildCanvas
-                        svgData={item}
-                        width={200}
-                        height={200}
-                        fillColor={fillColor}
-                        elements={elements}
-                        onElementChange={onElementChange}
-                        unitType={unitType}
-                      />
-                    ) : (
-                      <img
-                        src={item}
-                        alt={`Item ${index + 1}`}
-                        className="w-full h-full rounded-lg"
-                      />
-                    )}
-                  </div>
-                ) : (
-                  <div className="w-52 h-52 bg-gray-200 rounded-lg"></div>
-                )}
-              </div>
-            );
-          })}
+              return (
+                <div key={index} className="w-1/3">
+                  {item && typeof item === "string" ? (
+                    <div className="bg-gray-200 rounded-lg w-52 h-52">
+                      {item.includes("<svg") ? (
+                        <ChildCanvas
+                          svgData={item}
+                          width={200}
+                          height={200}
+                          fillColor={fillColor}
+                          elements={elements}
+                          onElementChange={onElementChange}
+                          unitType={unitType}
+                        />
+                      ) : (
+                        <img
+                          src={item}
+                          alt={`Item ${index + 1}`}
+                          className="w-full h-full rounded-lg"
+                        />
+                      )}
+                    </div>
+                  ) : (
+                    <div className="w-52 h-52 bg-gray-200 rounded-lg"></div>
+                  )}
+                </div>
+              );
+            })}
         </div>
         <Button
           onClick={onNextButtonClick}
