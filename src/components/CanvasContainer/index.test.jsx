f3800f76484aeb3f2b9cc2301e3de0a8b4615bdf
donghyukkil <asterism90@gmail.com>
2023-10-07 19:59:59 +0900

    test: CanvasContainer 테스트 추가

---
+++ src/components/CanvasContainer/index.test.jsx
@@ -0,0 +1,46 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, test, vi } from "vitest";
+
+import CanvasContainer from "./index";
+
+vi.mock("react-router-dom", () => ({
+  useNavigate: vi.fn(),
+}));
+
+describe("CanvasContainer Component", () => {
+  const mockStyle = "";
+  const mockElements = {
+    head: { svgData: "<svg></svg>", fillColor: "red" },
+    face: { svgData: "<svg></svg>", fillColor: "blue" },
+    body: { svgData: "<svg></svg>", fillColor: "yellow" },
+  };
+
+  beforeEach(() => {
+    render(<CanvasContainer style={mockStyle} elements={mockElements} />);
+  });
+
+  test("renders expected child components and elements", () => {
+    expect(screen.getByPlaceholderText("Title")).toBeInTheDocument();
+    expect(screen.getByText("Public")).toBeInTheDocument();
+  });
+
+  test("changes title in input field", () => {
+    const input = screen.getByPlaceholderText("Title");
+
+    fireEvent.change(input, { target: { value: "New Title" } });
+
+    expect(input.value).toBe("New Title");
+  });
+
+  test("toggles public/private", async () => {
+    const publicText = screen.getByText(/Public/i);
+
+    expect(publicText).toBeInTheDocument();
+
+    const toggleButton = screen.getByTestId(/toggleButton/i);
+    fireEvent.click(toggleButton);
+    const privateText = await screen.findByText(/Private/i);
+
+    expect(privateText).toBeInTheDocument();
+  });
+});
